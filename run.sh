96e777f2527bb95ddf2979f6b92d8e975175468f
Yash <yash@machine.local>
2025-06-27 15:27:57 +0530

    test topic

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt
-export ENV=${1:-dev}
+export ENV=${1:-stage}
 uvicorn app:app --host 0.0.0.0 --port 8000 --reload &
 python kafka/consumers/frames_consumer.py &
 
